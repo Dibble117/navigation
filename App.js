06dfca8896b6e6a82d68ccf823427d134821e225
Niko <t1koni02@students.oamk.fi>
2023-08-31 15:52:11 +0300

    navi app done

--- App.js
+++ App.js
@@ -1,12 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import HomeScreen from "./HomeScreen";
+import SecondScreen from "./SecondScreen";
+
+const Stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: 'Home',
+            headerTitle: 'Home',
+          }}
+        />
+        <Stack.Screen
+          name="Second"
+          component={SecondScreen}
+          options={{
+            title: 'Second',
+            headerTitle: 'Second',
+          }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ SecondScreen.js
@@ -0,0 +1,34 @@
+import React, { useLayoutEffect, useEffect } from "react";
+import { BackHandler, StyleSheet, Text, View } from "react-native";
+
+export default function SecondScreen({ route, navigation}) {
+
+    useEffect(() => {
+        if (route.params?.message) {
+            alert(route.params.message);
+        }
+        BackHandler.addEventListener("hardwareBackPress", close);
+        return() => {
+            BackHandler.removeEventListener("hardwareBackPress", close);
+        }
+    }, []);
+
+    function close() {
+        navigation.goBack(null);
+        return true;
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerStyle: {
+                backgroundColor: "#f0f0f0",
+            },
+        })
+    }, []);
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 10,
+    }
+});
